6a5d3041f547941d0b75f3b37bd08eda9800a316
CeKaGe <github@cekage.net>
2015-08-14 01:18:02 +0200

    cleaning MAGIC and make http_request kicked by BookletManager events

--- lib/filter_cvm.h
+++ lib/filter_cvm.h
@@ -16,8 +16,8 @@
     Illustrate filter.h, in this example we first check "cvm" by MAGIC
     then check "Suppleme"ntarInfoBox by MAGIC too.
 
-    Then filter with a expensive regex (which can be bypased in fact)
-    and then extract room / asin from SupplementarInfoBox
+    Then filters with a expensive regex (which can be avoided in fact)
+    and then (for example) extracts room / asin from SupplementarInfoBox
 */
 
 #define _GNU_SOURCE
@@ -29,17 +29,12 @@
 #include "filter.h"
 #endif
 
+#ifndef HTMLREQUEST_H
 #include "./htmlrequest.h"
+#endif
 
 // in string : 'cvm['
 #define MAGIC_CVM  0x5b6d7663
-
-// in string : 'Suppleme'
-#define MAGIC_SupplementarInfoBox 0x656d656c70707553
-
-// in string : 'BookletM'
-#define MAGIC_BookletManager 0x4d74656c6b6f6f42
-
 
 void process_SupplementarInfoBox(const char** line);
 
@@ -50,15 +45,17 @@ void process_BookletManager(const char** line);
 /* *****************************   SupplementarInfoBox    ******************************** */
 
 void process_SupplementarInfoBox(const char** line) {
+    // in string : 'Suppleme'
+    const uint64_t MAGIC = 0x656d656c70707553;
     if (strlen(*line) > 4 &&
-            check_MAGIC_32_64(line, MAGIC_CVM, MAGIC_SupplementarInfoBox)) {
-        const char* regexp_SupplementarInfoBox =
+            check_MAGIC_32_64(line, MAGIC_CVM, MAGIC)) {
+        const char* regexp_for_log =
             "SupplementarInfoBox:QUICK_LOOKUP:asin=.+,word=.+:$";
-        if (check_regexp(line, &regexp_SupplementarInfoBox)) {
+        if (check_regexp(line, &regexp_for_log)) {
             char* room;
             char* asin;
             char* url_request = NULL;
-            extractdata_only_two_keys(line, &asin, &room);
+            extractdata_only_2_keys(line, &asin, &room);
             if (-1 != asprintf(&url_request, "?toggle=%s", room)) {
                 //~ (void)printf("url_request=%s",url_request);
                 do_http_request(url_request);
@@ -75,15 +72,23 @@ void process_SupplementarInfoBox(const char** line) {
 /* *****************************   BookletManager    ******************************** */
 
 void process_BookletManager(const char** line) {
+    // in string : 'BookletM'
+    const uint64_t MAGIC = 0x4d74656c6b6f6f42;
     if (strlen(*line) > 47 &&
-            check_MAGIC_32_64(line, MAGIC_CVM, MAGIC_BookletManager)) {
-        const char* regexp_BookletManager =
+            check_MAGIC_32_64(line, MAGIC_CVM, MAGIC)) {
+        const char* regexp_for_log =
             "BookletManager:SwitchingBooklets:from=.+,to=.+:$";
-        if (check_regexp(line, &regexp_BookletManager)) {
+        if (check_regexp(line, &regexp_for_log)) {
             char* from;
             char* to;
-            extractdata_only_two_keys(line, &from, &to);
-            (void)printf("BookletManager user went to %s from %s\n", from, to);
+            char* url_request = NULL;
+            extractdata_only_2_keys(line, &from, &to);
+            if (-1 != asprintf(&url_request, "?from=%s&to=%s", from,to)) {
+                do_http_request(url_request);
+                free(url_request);
+            } else {
+                perror("process_BookletManager() can't build url_request");
+            }
             free(from);
             free(to);
         }
@@ -96,7 +101,7 @@ void process_BookletManager(const char** line) {
     //~ Old version with array instead of pointer.
     void process_SupplementarInfoBox(const char* line[STATIC_MAXGETLINE]) {
     if (strlen(*line) > 4 && check_MAGIC_SupplementarInfoBox(line)) {
-        if (check_regexp_SupplementarInfoBox(line)) {
+        if (check_regexp_for_log(line)) {
             char room[MAX_GETLINE + 1];
             char asin[MAX_GETLINE + 1];
             // I did'nt manage to pass asin & room as parameters to extractdata_
