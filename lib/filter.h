b952c5b70e83b6ebb683f1b067dc836c4678c57c
CeKaGe <github@cekage.net>
2015-08-13 15:18:15 +0200

    change array to ptr

--- lib/filter.h
+++ lib/filter.h
@@ -28,22 +28,14 @@
 #define FILTER_H
 #endif
 
-#define MAX_GETLINE 200
-
 #ifndef S_SPLINT_S
-// splint doesn't like static in array declaration…
-#define STATIC_MAXGETLINE static MAX_GETLINE+1
-#else
-#define STATIC_MAXGETLINE
 #endif
 
-bool check_regexp(const char* line[STATIC_MAXGETLINE],
-                  const char* regexp[STATIC_MAXGETLINE]);
-char* extract_substring(const char** src, char** dst, const char key_start,
-                        const char key_stop);
+bool check_regexp(const char** line, const char** filter);
+char* extract_substring(const char** src, char** dst,
+                        const char key_start, const char key_stop);
 
-bool check_regexp(const char* line[STATIC_MAXGETLINE],
-                  const char* filter[STATIC_MAXGETLINE]) {
+bool check_regexp(const char** line, const char** filter) {
     // Big waste of time, don't forget checkMAGIC before !!
     int reg_result;
     regex_t regex;
@@ -57,26 +49,34 @@ bool check_regexp(const char* line[STATIC_MAXGETLINE],
     return 0 == reg_result;
 }
 
-char* extract_substring(const char** src, char** dst, const char key_start,
-                        const char key_stop) {
+char* extract_substring(const char** src, char** dst,
+                        const char key_start, const char key_stop) {
     char* result = NULL;
     if ( NULL != *src) {
         char* cursor_start;
+        // Searching key_start in src
         cursor_start = strchr(*src, key_start);
         if ( NULL != cursor_start ) {
+            // Ok, cursor_start is in src and we point it
             char* cursor_end;
+            // inc cursort_start to be sure cursor_end will be superior
             ++cursor_start;
+            // Searching key_stop in src
             cursor_end = strchr(cursor_start, key_stop);
             if ( NULL != cursor_end ) {
-                int asprint_ret;
+                // Ok, cursor_end is in src and we point it
+                int asprint_ret;      // return of asprint
                 size_t cursor_length; // get the size of the inter-key word.
                 cursor_length = cursor_end - cursor_start;
                 //~ (void)printf("\nstart:[%s] end:[%s] size:[%zu]\n",
-                             //~ cursor_start, cursor_end, cursor_length);
+                //~ cursor_start, cursor_end, cursor_length);
                 //~ ** dst = '\0';  // emptying the destination string before filling it.
                 //~ strncat(*dst, cursor_start, MIN(cursor_length, MAX_GETLINE));
                 asprint_ret = asprintf((char** restrict) dst, "%s", cursor_start);
                 if (-1 != asprint_ret) {
+                    // No error occurs during asprintf so let truncate dst.
+                    // It's possible to truncate during asprintf by using %.Xs
+                    // but X is not predictible and needs more string manipulation.
                     *(*dst + cursor_length) = '\0';
                     // asprint + cutting dst after cursor_length bytes is more
                     // efficient on the kindle than calloc+strncat  ¯\_(ツ)_/¯
